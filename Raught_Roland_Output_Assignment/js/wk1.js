0d02bcbf388e42de600dafbf61e1616da4e72680
jnikko13 <jraught@gmail.com>
2015-07-10 22:54:36 -0400

    Adding something about me

--- Raught_Roland_Output_Assignment/js/wk1.js
+++ Raught_Roland_Output_Assignment/js/wk1.js
@@ -7,5 +7,12 @@
  */
 
 //testing connectivity
-var name = prompt("Let\'s test connectivity.\nPlease type in your name:");
-console.log(name);
+var name = prompt("Let\'s learn about Roland.\nPlease type in your name:");
+console.log("Welcome "+name+".");
+
+var aliasName = true; //Boolean variable
+
+var myName = "Jay";
+console.log("My real name is Roland. Since the name has been used four times, my mother used an initial in my full name to tell us apart!");
+
+
