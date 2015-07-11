4133fb76f75c219f96b4a64be84bc8e71b13054d
jnikko13 <jraught@gmail.com>
2015-07-10 23:07:35 -0400

    Tweaking the output

--- Raught_Roland_Output_Assignment/js/wk1.js
+++ Raught_Roland_Output_Assignment/js/wk1.js
@@ -12,7 +12,9 @@ console.log("Welcome "+name+".");
 
 var aliasName = true; //Boolean variable
 
+//added a string variable
 var myName = "Jay";
-console.log("My real name is Roland. Since the name has been used four times, my mother used an initial in my full name to tell us apart!");
+console.log("My real name is Roland D.J.Raught III. I go by "+myName+". Since the name has been used four times, my mother used J initial instead of Roland. My father goes by D.J. and my grandfather goes by Roland. The name comes from a family doctor named Roland Davis Jones.");
 
-
+var uncle = 8; //number variable
+console.log("I am an uncle of "+uncle+" nieces and nephews!!");
