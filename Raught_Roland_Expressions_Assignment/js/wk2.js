02d1246932abebcd3bb72a9f192ccfb2e48352c7
jnikko13 <jraught@gmail.com>
2015-07-16 21:31:35 -0400

    Added code and test connectivity

--- Raught_Roland_Expressions_Assignment/js/wk2.js
+++ Raught_Roland_Expressions_Assignment/js/wk2.js
@@ -6,3 +6,15 @@
  *
  */
 
+//Creating a Calculator to determine toll cost
+var name = prompt("Let\'s figure out how much it will cost in tolls for a trip to the beach. \nPlease type in your name:");
+console.log(name);
+
+//Alert prompt to say hello
+alert("Hello "+name+".");
+
+//To calc the cost we need to ask how many tolls they will encounter on their trip.
+var tollStops =prompt("How many tolls will you encounter on this trip?");
+
+//console.log answer
+console.log(tollStops);
