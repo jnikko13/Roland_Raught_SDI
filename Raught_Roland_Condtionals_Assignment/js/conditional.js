dbd96188d2b6020c2decd7dc59d0d0c25890b7c9
jnikko13 <jraught@gmail.com>
2015-07-17 19:30:08 -0400

    Conditionals file setup

---
+++ Raught_Roland_Condtionals_Assignment/js/conditional.js
@@ -0,0 +1,6 @@
+/**
+ * Created by panzer on 7/17/15.
+ */
+
+var name = prompt("Let\'s figure out how much it will cost in tolls for a trip to the beach. \nPlease type in your name:");
+console.log(name);
