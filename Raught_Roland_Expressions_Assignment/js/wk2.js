d6dcb56eb865fe1bc719b5dca141bbc4a8874a93
jnikko13 <jraught@gmail.com>
2015-07-16 21:41:19 -0400

    Added test comments

--- Raught_Roland_Expressions_Assignment/js/wk2.js
+++ Raught_Roland_Expressions_Assignment/js/wk2.js
@@ -44,3 +44,19 @@ console.log(weekendPrice);
 //Final output using console.log and alert values to show prices
 console.log(name+", If you travel on the weekend, it will now cost you $"+weekendPrice+" at the adjusted price of $"+newRate+" per toll to drive this route.");
 alert(name+", Weekend travel will cost $"+weekendPrice+" at the adjusted rate $"+newRate+" per toll.");
+
+/*
+ *Tested rates @ 2.00 per toll weekday travel and stop @ 4 tolls
+ $8.00 per weekday trip
+ Changed rate to $4.50 per toll weekend travel and stops @ 4 tolls
+ $18.00 per weekend trip
+
+ Tested price @ 3.75 per toll for weekday travel and 8 tolls
+ $30.00 per weekday trip
+ Changed price to 5.75 per toll for weekend travel and 8 tolls
+ $46.00 per weekend trip
+
+ Values were also tested via calculator
+
+ */
+
