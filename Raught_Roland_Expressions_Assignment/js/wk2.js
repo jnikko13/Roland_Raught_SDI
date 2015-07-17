84606c9ea4c21cc850e881869e129fb58de321ae
jnikko13 <jraught@gmail.com>
2015-07-16 21:34:18 -0400

    Added weekday toll rates

--- Raught_Roland_Expressions_Assignment/js/wk2.js
+++ Raught_Roland_Expressions_Assignment/js/wk2.js
@@ -18,3 +18,15 @@ var tollStops =prompt("How many tolls will you encounter on this trip?");
 
 //console.log answer
 console.log(tollStops);
+
+//calculate the cost of tolls
+
+//Asking user for current toll rates
+var weekdayPrice = 2.75;
+
+//calculate the current toll rates
+var price =weekdayPrice * tollStops;
+
+//alert with weekday toll rates
+alert(name+", It will cost you $"+price+" in tolls for this trip at the weekday rate of $"+weekdayPrice+" per toll.");
+console.log(name+", Total cost is $"+price+" at the weekday toll rate $"+weekdayPrice+" per toll.");
